3f145331dd7c172fc89a26ee39ee3e26c8363e21
Maxwell Hadley <mch6706@rit.edu>
2015-05-08 19:56:06 -0400

    Update TimerObserver.java

--- Checkers/src/TimerObserver.java
+++ Checkers/src/TimerObserver.java
@@ -1,10 +1,24 @@
 import java.util.Observer;
 
-
+/**
+ * This interface is used for implementing the observer pattern
+ * for a timer and to make sure the observer has the proper
+ * functionality
+ */
 public interface TimerObserver extends Observer {
 
+	/**
+	 * This function is used to notify when a player
+	 * has run out of time for their turn
+	 */
 	public void notifyTimeUp();
 	
+	/**
+	 * This function is used to notify the observers 
+	 * of a time left change to that any resulting
+	 * activities can be activated I.E. updating the 
+	 * timer label
+	 */
 	public void notifyUpdateTime(int newTimeLeft);
 	
 }

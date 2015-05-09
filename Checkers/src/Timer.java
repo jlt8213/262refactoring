3fc679fb4c212d1210e8fd5d0aa4e038186159f6
Maxwell Hadley <mch6706@rit.edu>
2015-05-08 20:02:52 -0400

    Update Timer.java

--- Checkers/src/Timer.java
+++ Checkers/src/Timer.java
@@ -1,40 +1,57 @@
 import java.util.ArrayList;
 /**
  *  This timer runs on the separate thread and during
- *  the simulation tells fasade to update the warning and normal times 
- *
- *  @invariant all variables have valid values
- *
- *  @author
+ *  the simulation tells facade to update the warning and normal times
  */
 
 public class Timer extends Thread{
 
+    /**The time limit for a player's single turn*/
     private long timeLimit;
+    /**This stores the last time there was a change of turns*/
     private long lastTurnSwap;
+    /**This is used for continuing the timer loop until the end of the game*/
     private boolean running;
+    /**This is a list of all the observers that need to be updated of timer changes*/
     private ArrayList<TimerObserver> observers;
 	
+	/**This constructor creates a new Timer with the given timelimit
+	 * and no default observers
+	 */
 	public Timer(long timeLimit){
     	super();
     	init(timeLimit, null);
     }
     
+    /**This constructor creates a new Timer with the default time limit
+	 * and no default observers
+	 */
     public Timer(){
     	super();
     	init(30000L, null);
     }
     
+    /**This constructor creates a new Timer with the given timelimit
+	 * and the given default observer
+	 */
     public Timer(TimerObserver initialTimerObserver){
     	super();
     	init(30000L, initialTimerObserver);
     }
     
+    /**This constructor creates a new Timer with the default time limit
+	 * and the given default observer
+	 */
     public Timer(long timeLimit, TimerObserver initialTimerObserver){
     	super();
     	init(timeLimit, initialTimerObserver);
     }
     
+    /**
+     * This function is the init used by all the contructors to 
+     *  - Set the time limit, default or not
+     *  - Add the initial observer if it is not null
+     */
     private void init(long timeLimit, TimerObserver initialTimerObserver){
     	this.timeLimit = timeLimit;
     	this.observers = new ArrayList<TimerObserver>(1);
@@ -43,10 +60,16 @@ public class Timer extends Thread{
     	}
     }
     
+    /**
+     * Used to add more observers of the timer after the creation of it
+     */
     public void addTimerObserver(TimerObserver newTimerObserver){
     	this.observers.add(newTimerObserver);
     }
     
+    /**
+     * This function force stops the running of the timer
+     */
     public void stopRunning(){
     	this.running = false;
     }
@@ -75,6 +98,9 @@ public class Timer extends Thread{
     	}
     }
     
+    /**
+     * This function resets the timer on a turn swap 
+     */
     public void reset(){
         lastTurnSwap = System.currentTimeMillis();
     }
